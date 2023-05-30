b0c74333627f4ac8f0cf70d4fb7f5d7ff52f90b2
Skythrew <mael.guerin@outlook.fr>
2023-05-30 19:48:20 +0200

    feat: add help command

--- stmk.sh
+++ stmk.sh
@@ -5,3 +5,23 @@
 # License: GNU GENERAL PUBLIC LICENSE v3     #
 ##############################################
 
+print_help() {
+    echo "========================STMK========================"
+    echo "| Help:                                            |"
+    echo "===================================================="
+    echo ""
+    echo "stmk -k => Keep build files"
+    echo "stmk -v => Show logs on current terminal"
+    echo "stmk help | -h => Show this help menu"
+    echo ""
+}
+
+# CLI parser
+case "$1" in
+    "help")
+        print_help
+        ;;
+    "-h")
+        print_help
+        ;;
+esac
