a4df7261e3d6b11c42d1f2e34b1ba0fb8d6e84eb
Skythrew <mael.guerin@outlook.fr>
2023-05-31 18:24:43 +0200

    feat: add help menu

--- scripts/stadd/stadd.sh
+++ scripts/stadd/stadd.sh
@@ -5,3 +5,17 @@
 # License: GNU GENERAL PUBLIC LICENSE v3     #
 ##############################################
 
+source "$(dirname -- "$0")/../utils.sh"
+
+print_help() {
+    print_header "Help:"
+    print_content ""
+    print_content "stadd <package_file>"
+    print_content ""
+    print_header_end
+}
+
+# CLI Parser
+if [ "$1" == "help" ] || [ "$1" == "-h" ]; then
+    print_help
+fi
