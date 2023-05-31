68c16c9f9779a94ab59b61ac934724226a498719
Skythrew <mael.guerin@outlook.fr>
2023-05-31 18:00:56 +0200

    feat: add utils script

---
+++ scripts/utils.sh
@@ -0,0 +1,31 @@
+print_header() {
+    echo "========================STMK========================"
+    spaces=$((48 - ${#1}))
+    printf "| $1 %${spaces}s|\n"
+}
+
+print_content() {
+    spaces=$((48 - ${#1}))
+    printf "| $1%${spaces}s |\n"
+}
+
+print_header_end() {
+    echo "===================================================="
+}
+
+print_help() {
+    print_header "Help:"
+    print_content ""
+    print_content "stmk -k => Keep build files"
+    print_content "stmk help | -h => Show this help menu"
+    print_content ""
+    print_content ""
+    print_content "Environment variables:"
+    print_content ""
+    print_content "VERBOSE=bool  => Show build output ?"
+    print_header_end
+}
+
+print_error() {
+    echo -e "\e[1;31m$1\e[0m"
+}
