b44099b5e3d6b67c7b06bdaf2acb63e4834cc820
Skythrew <mael.guerin@outlook.fr>
2023-05-31 19:59:58 +0200

    feat: add print_info and print_success

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -26,6 +26,14 @@ print_help() {
     print_header_end
 }
 
+print_info() {
+    echo -e "\e[1;34m$1\e[0m"
+}
+
 print_error() {
     echo -e "\e[1;31m$1\e[0m"
+}
+
+print_success() {
+    echo -e "\e[1;32m$1\e[0m"
 }
