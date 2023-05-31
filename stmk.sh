5a2164177ad927053cab5bd16444a6cf91afb096
Skythrew <mael.guerin@outlook.fr>
2023-05-31 09:06:21 +0200

    feat: add unpack,build and pack functions and source download

--- stmk.sh
+++ stmk.sh
@@ -30,6 +30,32 @@ print_help() {
     echo ""
 }
 
+unpack() {
+    tar -xf $name-$version.tar.*
+    if [ -d $name-$version ]; then
+        cd $name-$version
+    fi
+}
+
+build() {
+    ./configure --prefix=/usr --disable-static
+    make
+    make DESTDIR=$PKG install
+}
+
+pack() {
+    cd $PKG
+    # Remove unneeded and harmful LA files
+    if [ $KEEP_LA==true ]; then
+        for file in $(find $directory -type f -name "*.la");
+        do
+            echo $file
+        done
+    fi
+
+    tar -I 'zstd --ultra -22' -cf ../$name-$version.tar.zst $PKG
+}
+
 # CLI parser
 case "$1" in
     "help")
@@ -37,5 +63,25 @@ case "$1" in
         ;;
     "-h")
         print_help
+        ;;
+    *)
+        source ./recipe
+        WORKDIR=$(mktemp -d)
+        source=(http://downloads.nutyx.org/x86_64/rolling/base/acl/acl1626618727x86_64.cards.tar.xz http://downloads.nutyx.org/x86_64/rolling/base/attr/attr1626618688x86_64.cards.tar.xz#attr.tar.xz)
+        cd $WORKDIR
+        for sourceURL in ${source[@]};
+        do
+            URL=$(echo ${sourceURL} | cut -d "#" -f 1)
+            if [ $(echo ${sourceURL} | cut -d "#" -f 2) == $URL ]; then
+                FILENAME=${sourceURL##*/}
+            else
+                FILENAME=$(echo ${sourceURL} | cut -d "#" -f 2)
+            fi
+
+            echo $URL
+            echo $FILENAME
+            echo ""
+            curl -o $FILENAME $URL
+        done
         ;;
 esac
