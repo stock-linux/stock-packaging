c90d20618c99af10e978af48a841aaf8582af184
Skythrew <mael.guerin@outlook.fr>
2023-05-31 17:58:54 +0200

    project: init stadd

---
+++ scripts/stadd/stadd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################### STMK ####################
+# Packaging system of Stock Linux distro     #
+# License: GNU GENERAL PUBLIC LICENSE v3     #
+##############################################
