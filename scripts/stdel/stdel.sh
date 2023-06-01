9d42c239a4279bcb0d8e8af2fb3c2bd0e70de7c9
Skythrew <mael.guerin@outlook.fr>
2023-06-01 16:30:06 +0200

    project: init stdel script

---
+++ scripts/stdel/stdel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#################### STDEL ####################
+# Packaging system of Stock Linux distro    #
+# License: GNU GENERAL PUBLIC LICENSE v3     #
+##############################################
+
+source "$(dirname -- "$0")/../utils.sh"
