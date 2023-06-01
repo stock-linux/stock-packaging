7ad704982f2646ff18c1e74feef5b0fbc34289bc
Skythrew <mael.guerin@outlook.fr>
2023-06-01 17:29:45 +0200

    feat: add help menu

--- scripts/stdel/stdel.sh
+++ scripts/stdel/stdel.sh
@@ -6,3 +6,16 @@
 ##############################################
 
 source "$(dirname -- "$0")/../utils.sh"
+
+print_help() {
+    print_header "Help:"
+    print_content ""
+    print_content "stdel <package_name>"
+    print_content ""
+    print_header_end
+}
+
+# CLI Parser
+if [ "$1" == "help" ] || [ "$1" == "-h" ]; then
+    print_help
+fi
