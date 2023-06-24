6dba2cc47c891888309851b404953aa87ad855e7
Skythrew <mael.guerin@outlook.fr>
2023-06-24 19:38:06 +0200

    feat: finish stdel

--- scripts/stdel/stdel.sh
+++ scripts/stdel/stdel.sh
@@ -16,3 +16,39 @@ print_error() {
 print_success() {
     echo -e "\e[1;32m$1\e[0m"
 }
+
+if [ "$ROOT" == "" ]; then
+    ROOT="/"
+fi
+
+cd $ROOT
+
+if [ ! -d $ROOT/var/packages/$PACKAGE_NAME ] && [ "$2" != "-u" ]; then
+    print_error "Package $PACKAGE_NAME is not installed !"
+    exit 1
+fi
+
+print_info "Deleting package $PACKAGE_NAME..."
+dirs_to_remove=()
+for line in $(cat $ROOT/var/packages/$PACKAGE_NAME/FILETREE);
+do
+    if [ "$line" == "./.FILETREE"* ] || [ "$line" == "./.PKGINDEX"* ] || [ "$line" == "." ]; then
+        continue
+    fi
+
+    if [ -d $line ]; then
+        dirs_to_remove+=($line)
+    else
+        rm -f $line
+    fi
+done
+
+for dir in ${dirs_to_remove[@]};
+do
+    if [ -z "$(ls -A -- "$dir")" ]; then
+        rm -d $dir
+    fi
+done
+
+rm -rf $ROOT/var/packages/$PACKAGE_NAME
+print_success "Done !"
